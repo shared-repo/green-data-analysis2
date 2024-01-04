44df19d5e55a7ac7c620d803c76fc2b54154cca2
instructor <instructor@example.com>
2024-01-04 15:17:48 +0900

    update code

--- workspace/db/07.ddl.sql
+++ workspace/db/07.ddl.sql
@@ -60,7 +60,7 @@ create table emp_temp
 );
 drop table emp_temp;
 
--- 직접 테이블 구조를 지정해서 테이블 생성 2
+-- 직접 테이블 구조를 지정해서 테이블 생성 3
 create table emp_temp
 (
     -- 컬럼이름 자료형 [null?] [기본키] [외래키] [check] [default-value]
@@ -83,6 +83,86 @@ create table emp_temp
 insert into emp_temp (empno)
 values (1);
 select * from emp_temp;
+drop table emp_temp;
 
+-- 직접 테이블 구조를 지정해서 테이블 생성 4
+create table emp_temp
+(
+    -- 컬럼이름 자료형 [null?] [기본키] [외래키] [check] [default-value]
+    -- empno number(4,0) not null primary key, -- 기본키 지정
+    empno number(4,0) not null,
+    ename varchar2(10 byte) null, -- null 허용 (default)
+    job varchar2(9 byte),
+    mgr number(4,0), -- 자기 참조 테이블의 foreign key는 null 허용
+    hiredate date default(sysdate),
+    sal number(7,2),
+    comm number(7,2),
+    deptno number(2,0) not null, -- 보통 foreign key column은 not null로 지정
+    -- primary key (empno) -- empno를 pk로 지정
+    constraint pk_emp_temp primary key (empno), -- empno를 pk로 지정
+    constraint fk_emp_temp_to_dept -- 이 행은 생략 가능
+    foreign key (deptno) references dept(deptno),
+    constraint fk_emp_temp_to_emp_temp -- 이 행은 생략 가능
+    foreign key (mgr) references emp_temp(empno)
+);
+insert into emp_temp(empno, deptno)
+values (1, 10); -- deptno는 반드시 dept 테이블에 존재하는 값을 사용해야 합니다.
+select * from emp_temp;
+commit;
+delete from dept where deptno = 10; -- 다른 테이블에서 참조하고 있는 데이터는 삭제 불가능
+
+drop table emp_temp;
+
+-- 직접 테이블 구조를 지정해서 테이블 생성 5
+create table emp_temp
+(
+    -- 컬럼이름 자료형 [null?] [기본키] [외래키] [check] [default-value]
+    -- empno number(4,0) not null primary key, -- 기본키 지정
+    empno number(4,0) not null,
+    ename varchar2(10 byte) null, -- null 허용 (default)
+    job varchar2(9 byte),
+    mgr number(4,0), -- 자기 참조 테이블의 foreign key는 null 허용
+    hiredate date default(sysdate),
+    sal number(7,2) check(sal > 0),
+    comm number(7,2),
+    deptno number(2,0) not null, -- 보통 foreign key column은 not null로 지정
+    -- primary key (empno) -- empno를 pk로 지정
+    constraint pk_emp_temp primary key (empno), -- empno를 pk로 지정
+    constraint fk_emp_temp_to_dept -- 이 행은 생략 가능
+    foreign key (deptno) references dept(deptno),
+    constraint fk_emp_temp_to_emp_temp -- 이 행은 생략 가능
+    foreign key (mgr) references emp_temp(empno)
+);
+insert into emp_temp (empno, deptno, sal)
+values (1, 10, -1000); -- check 제약조건 위반 insert 실패
+select * from emp_temp;
+
+drop table emp_temp;
+
+-- 직접 테이블 구조를 지정해서 테이블 생성 6
+create table emp_temp
+(
+    -- 컬럼이름 자료형 [null?] [기본키] [외래키] [check] [default-value]
+    -- empno number(4,0) not null primary key, -- 기본키 지정
+    empno number(4,0) not null,
+    ename varchar2(10 byte) null, -- null 허용 (default)
+    job varchar2(9 byte),
+    mgr number(4,0), -- 자기 참조 테이블의 foreign key는 null 허용
+    hiredate date default(sysdate),
+    sal number(7,2) check(sal > 0),
+    comm number(7,2),
+    deptno number(2,0) not null, -- 보통 foreign key column은 not null로 지정
+    -- primary key (empno) -- empno를 pk로 지정
+    constraint pk_emp_temp primary key (empno), -- empno를 pk로 지정
+    constraint fk_emp_temp_to_dept -- 이 행은 생략 가능
+    foreign key (deptno) references dept(deptno),
+    constraint fk_emp_temp_to_emp_temp -- 이 행은 생략 가능
+    foreign key (mgr) references emp_temp(empno)
+);
+create sequence emp_temp_sequence;
+select emp_temp_sequence.nextval from dual;
+insert into emp_temp(empno, deptno, sal)
+values (emp_temp_sequence.nextval, 10, 1000);
+select * from emp_temp;
 
 
